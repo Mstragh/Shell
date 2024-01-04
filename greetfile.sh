2f011e74e66e287bec3ca560738989864ec7f5d1
Mstragh <155651236+Mstragh@users.noreply.github.com>
2024-01-04 21:22:06 +0530

    Create greetfile.sh
    ## Greetings for all *Users* ##

---
+++ greetfile.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "hello, User..!"
+echo "\nI am Mstragh."
+echo "\nThis is my Script."
+echo "\nWhat is your Name ?"
+read PERSON
+echo "\nhello, $PERSON "
+echo "\nToday is $DATE "
